044825a478b75190c11134042aa1835ea3d6477e
Murat Akkus <you@example.com>
2024-09-19 16:50:10 +0200

    initial commit

---
+++ assets/js/slider.js
@@ -0,0 +1,24 @@
+const progressContent = document.querySelector(".autoplay-progress span");
+
+var swiper = new Swiper(".mySwiper", {
+    
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    loop:true,
+    lazy:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+
+});
